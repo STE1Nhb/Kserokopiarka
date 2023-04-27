b9bd3c74cfd6b8c76054bc6a082be2d80656f74b
arseni.pahuda <arseni.pahuda@BR-16>
2023-04-27 12:27:49 +0200

    Dodaj pliki projektów.

---
+++ Zadanie 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using v1 = ver1;
+
+namespace ver1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+
+            xerox.ScanAndPrint();
+            System.Console.WriteLine(xerox.Counter);
+            System.Console.WriteLine(xerox.PrintCounter);
+            System.Console.WriteLine(xerox.ScanCounter);
+        }
+    }
+}
